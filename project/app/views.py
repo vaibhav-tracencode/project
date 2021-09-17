7fb6cdc22bdce5ec22d01a2a0b565489cedf2910
vaibhav <vaibhav.tracencode@gmail.com>
2021-09-17 20:53:10 +0530

    updated files

--- project/app/views.py
+++ project/app/views.py
@@ -5,6 +5,12 @@ from django.http import HttpResponse
 def index(request):
     return render(request,"app/index.html")
 
+def app_email_inbox(request):
+    return render(request,"app/apps-email-inbox.html")
+
+def app_email_read(request):
+    return render(request,"app/apps-email-read.html")
+
 
 def app_chat(request):
     return render(request,"app/apps-chat.html")
@@ -102,7 +108,7 @@ def ui_dropdown(request):
     return render(request,"app/ui-dropdowns.html")
 
 def ui_grid(request):
-    return render(request,"app/ui-grid.html")
+    return render(request,"app/ui-grids.html")
 
 def ui_images(request):
     return render(request,"app/ui-images.html")
@@ -138,7 +144,7 @@ def ui_toasts(request):
     return render(request,"app/ui-toasts.html")
 
 def ui_typography(request):
-    return renderI(request,"app/ui-typography.html")
+    return render(request,"app/ui-typography.html")
 
 def ui_videos(request):
     return render(request,"app/ui-videos.html")
@@ -147,7 +153,7 @@ def ui_widgets(request):
     return render(request,"widgets.html")
 
 def advance_animation(request):
-    return render(request,"advanced-animation.html")
+    return render(request,"app/advanced-animation.html")
 
 def advance_clipboard(request):
     return render(request,"app/advanced-clipboard.html")
@@ -181,3 +187,141 @@ def advance_session(request):
 def advance_sweetalerts(request):
     return render(request,"app/advanced-sweetalerts.html")
 
+def form_advance(request):
+    return render(request,"app/forms-advanced.html")
+
+def form_editors(request):
+    return render(request,"app/forms-editors.html")
+
+def form_elements(request):
+    return render(request,"app/forms-elements.html")
+
+def form_repeater(request):
+    return render(request,"app/forms-repeater.html")
+
+def form_uploads(request):
+    return render(request,"app/forms-uploads.html")
+
+def form_validation(request):
+    return render(request,"app/forms-validation.html")
+
+def form_wizard(request):
+    return render(request,"app/forms-wizard.html")
+
+def form_editable(request):
+    return render(request,"app/forms-x-editable.html")
+
+def horizontal_index(request):
+    return render(request,"app/horizontal-index.html")
+
+def horizontal_starter(request):
+    return render(request,"app/horizontal-starter.html")
+
+def horizontal_dark(request):
+    return render(request,"app/horizontal-top-dark.html")
+
+
+def page_blogs(request):
+    return render(request,"app/pages-blogs.html")
+
+def page_faq(request):
+    return render(request,"app/pages-faqs.html")
+
+def page_price(request):
+    return render(request,"app/pages-pricing.html")
+
+def page_profile(request):
+    return render(request,"app/pages-profile.html")
+
+def page_starter(request):
+    return render (request,"app/pages-starter.html")
+
+def page_timeline(request):
+    return render(request,"app/pages-timeline.html")
+
+def page_treeview(request):
+    return render(request,"app/pages-treeview.html")
+
+def sales_index(request):
+    return render(request,"app/sales-index.html")
+
+def table_basic(request):
+    return render(request,"app/tables-basic.html")
+
+def table_datatable(request):
+    return render(request,"app/tables-datatable.html")
+
+def table_editable(request):
+    return render(request,"app/tables-editable.html")
+
+def table_responsive(request):
+    return render(request,"app/tables-responsive.html")
+
+def icon_dripicons(request):
+    return render(request,"app/icons-dripicons.html")
+
+def icon_feather(request):
+    return render(request,"app/icons-feather.html")
+
+def icon_fontawesome(request):
+    return render(request,"app/icons-fontawesome.html")
+
+def icon_materialdesign(request):
+    return render(request,"app/icons-materialdesign.html")
+
+def icon_themify(request):
+    return render(request,"app/icons-themify.html")
+
+def icon_typicons(request):
+    return render(request,"app/icons-typicons.html")
+
+def map_google(request):
+    return render(request,"app/maps-google.html")
+
+def map_leaflet(request):
+    return render(request,"app/maps-leaflet.html")
+
+def map_vector(request):
+    return render(request,"app/maps-vector.html")
+
+def email_alert(request):
+    return render(request,"app/email-templates-alert.html")
+
+def email_basic(request):
+    return render(request,"app/email-templates-basic.html")
+
+def email_billing(request):
+    return render(request,"app/email-templates-billing.html")
+
+def chart_apex(request):
+    return render(request,"app/charts-apex.html")
+
+def chart_flot(request):
+    return render(request,"app/charts-flot.html")
+
+def chart_js(request):
+    return render(request,"app/charts-chartjs.html")
+
+def chart_morris(request):
+    return render(request,"app/charts-morris.html")
+
+def dark_horizontal_index(request):
+    return render(request,"app/dark-horizontal-index.html")
+
+def dark_index(request):
+    return render(request,"app/dark-index.html")
+
+def demo_dark_horizontal(request):
+    return render(request,"app/demo-dark-horizontal.html")
+
+def demo_dark(request):
+    return render(request,"app/demo-dark.html")
+
+def demo_navy(request):
+    return render(request,"app/demo-navy.html")
+
+def demo_rtl(request):
+    return render(request,"app/demo-rtl.html")
+
+def demo_rtl_horizontal(request):
+    return render(request,"app/demo-rtl-horizontal.html")
